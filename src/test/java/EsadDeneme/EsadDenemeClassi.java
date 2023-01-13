13830f71bcd2499f091a62bf3afd4d52b39a11c1
Esad <yildizme4@gmail.com>
2023-01-13 21:23:07 +0100

    last denemedir.

---
+++ src/test/java/EsadDeneme/EsadDenemeClassi.java
@@ -0,0 +1,12 @@
+package EsadDeneme;
+
+public class EsadDenemeClassi {
+    public static void main(String[] args) {
+
+        System.out.println("Bu bir deneme paketidir");
+
+        System.out.println("Bu 2.denemedir");
+
+
+    }
+}
